fe0c713928c1361fa9a1bce686f204af753eb487
Craig Minihan <craig@ripcordsoftware.com>
2017-02-21 15:41:51 +0000

    Update avancedb.rb

--- avancedb.rb
+++ avancedb.rb
@@ -1,7 +1,7 @@
 class Avancedb < Formula
   desc "An in-memory database based on the CouchDB REST API and containing the CouchDB Futon and Fauxton web sites"
   homepage "https://github.com/RipcordSoftware/AvanceDB"
-  url "https://github.com/RipcordSoftware/AvanceDB/archive/v0.1.0.tar.gz"  
+  url "ftp://ftp.ripcordsoftware.com/pub/avancedb/avancedb-0.1.1.tar.gz"  
   sha256 "e99ade1744bf6c0af43d8ae62fff24768c6380359e199e96e251fa7974c4876e"
   head "https://github.com/RipcordSoftware/AvanceDB.git"
 
