b7a8a29e7e6bea8ce7e7ad30698467e586064a58
Craig Minihan <craig@ripcordsoftware.com>
2017-02-21 19:06:27 +0000

    Remove the sha from avancedb

--- avancedb.rb
+++ avancedb.rb
@@ -2,7 +2,6 @@ class Avancedb < Formula
   desc "An in-memory database based on the CouchDB REST API and containing the CouchDB Futon and Fauxton web sites"
   homepage "https://github.com/RipcordSoftware/AvanceDB"
   url "https://github.com/RipcordSoftware/AvanceDB.git", :using => :git, :tag => "0.1.1"
-  sha256 "5900cba31fba8051f8eb741b3cef04647e2019f5c60e4c5ea09bd45f1b1a264f"
   head "https://github.com/RipcordSoftware/AvanceDB.git"
 
   depends_on "boost"
